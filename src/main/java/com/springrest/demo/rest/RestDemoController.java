3dc1b309c71954a2fb1309156cc95a8b90e996c9
pbukki <philipbukkie@gmail.com>
2023-06-29 21:26:16 +0300

    implemented exception handling

--- src/main/java/com/springrest/demo/rest/RestDemoController.java
+++ src/main/java/com/springrest/demo/rest/RestDemoController.java
@@ -1,4 +1,4 @@
-package com.springrest.demo;
+package com.springrest.demo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
